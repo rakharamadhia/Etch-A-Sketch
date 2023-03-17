a8550fd362c2e81564e9daba077af594ef1c0ce0
rakharamadhia <rakharamadhia@gmail.com>
2023-03-17 08:48:41 +0700

    add turn left and right

--- main.py
+++ main.py
@@ -12,8 +12,17 @@ def move_backward():
     joni.back(15)
 
 
+def turn_left():
+    joni.left(90)
+
+
+def turn_right():
+    joni.right(90)
+
+
 screen.listen()
 screen.onkey(move_forward, "Up")
 screen.onkey(move_backward, "Down")
-
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
 screen.exitonclick()
